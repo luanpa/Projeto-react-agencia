075b9a5eecdcc037309aab2b35f3860329c1f546
Luan Batista <69101674+luanpa@users.noreply.github.com>
2022-12-05 06:34:30 -0300

    completo com Html no backend

--- Desktop/db-website/mernbackend/src/app.js
+++ Desktop/db-website/mernbackend/src/app.js
@@ -1,12 +1,16 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
 const app = express ()
 const port = process.env.PORT || 3000;
 require("./db/conn")
 
+const static_path = path.join(__dirname, "./public" )
 
-app.get('/'), (req, res) => {
-    re.send("hello from the thapatechnical")
-}
+app.use(express.static(static_path));
+
+app.get("/", (req, res) => {
+    res.send("hello from the thapatechnical")
+ });
 
 app.listen(port, () => {
     console.log(`serve esta rodando na port ${port}`);
